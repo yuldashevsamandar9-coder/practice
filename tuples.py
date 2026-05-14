a5fdf6b49e9f482869b477f656f36012cae93d44
Sam <yuldashevsamandar9@gmail.com>
2026-05-14 23:59:47 +0900

    feat: ZIP endi urgandim

--- tuples.py
+++ tuples.py
@@ -73,3 +73,12 @@ def greeting(*args, **kwargs):
 
 
 greeting("Hi", True, 10, name="Neo", age=29)
+
+print("======  zip ======")
+tuple1 = (1, 2, 3, 4)
+tuple2 = ('a', 'b', 'c')
+
+zipped = zip(tuple1, tuple2)
+print("zipped:", zipped)
+result = list(zipped)
+print(f"the result: {result}")
