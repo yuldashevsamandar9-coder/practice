9d062718fb870883d7b2b801e6760a82767c197c
Sam <yuldashevsamandar9@gmail.com>
2026-05-14 23:01:38 +0900

    feat: list and tuple learn

---
+++ tuples.py
@@ -0,0 +1,35 @@
+'''
+Tuple
+(1) What is tuple: typle vs list
+(2) Unpacking arguments
+(3) zip
+
+'''
+
+print("======  What is tuple: typle vs list ======")
+# Java / PHP / NodeJS array => Python list,
+
+# literal
+numbs = [3, 5, 6, 7]
+car_dict = {"brand": "Ferrari", "year": 1996}
+print(numbs)
+
+
+# constructor
+letters = list("Hello World!")
+person_dict = dict(name="SAM", AGE=29)
+print(letters)
+
+
+fruits = ["apple", "banana", "kiwi"]
+print(fruits)
+fruits[1] = "nok"
+print(fruits)
+
+# tuple
+# we can not mutate tuple
+
+animals = ("dog", "cat", "fish")
+typle_obj = ("MIT", 100, True)
+print(animals[0])
+animals[0] = "bird"
